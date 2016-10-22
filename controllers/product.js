80bb8e17c00b08e3dfc92dd5a94f2478d34f6218
Yetzin <yillanes@ucol.mx>
2016-10-21 20:47:26 -0500

    sumer commitx2

--- controllers/product.js
+++ controllers/product.js
@@ -2,16 +2,19 @@ var fs = require('fs');
 var Product = require('../model/products.js').product;
 var Busboy = require('busboy');
 
+/*
+Upload files
+*/
 module.exports.upload = function(req, res){
   var timestamp = Date.now();
-  var busboy = new Busboy({headers: req.headers});
-  busboy.on('field', function(key, value){
-    console.log(key, value);
-  });
-  busboy.on('file', function(fieldname, file, filename){
+  var busboy    = new Busboy({headers: req.headers});
+
+  busboy.on('field', function(key, value){ console.log(key, value); });
+  busboy.on('file' , function(fieldname, file, filename){
     var stream = fs.createWriteStream('./public/img/products/'+timestamp+'_'+filename);
     file.pipe(stream);
   });
+  
   busboy.on('finish', function(){
     return  res.status(201).send('OK');
   });
